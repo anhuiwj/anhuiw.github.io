05a1f9ae40af1378ab3c5444fa3b8cb40de858e6
wangjie <wangjie@wondersgroup.com>
2017-08-02 17:45:50 +0800

    加入站长统计

---
+++ qjs/countNum.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wangjie on 2017/8/2.
+ */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?223173410c1a2706ab4d074f1d099837";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
